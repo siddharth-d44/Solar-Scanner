201481e7e0f038a3e7dedaf00a8502bcd06a3ae7
Siddharth Deshpande <siddharth_d@icloud.com>
2025-07-06 20:28:39 -0500

    This is my code for my solar scanner Arduino Project

---
+++ solarScannerCode/solarScannerCode.ino
@@ -0,0 +1,29 @@
+#include <Servo.h>
+
+// creates a servo object
+Servo myServo;
+
+
+// declarations and initalizations for the ultrasonic sensor
+
+int trig = 10; // trig pin on pin 10
+int echo = 11; // trig pin on pin 11
+long dur; // variable for the time duration of the high signal
+float dist; // variable for the distance calculated
+
+int buzz = 9; // passive buzzer on pin 9
+int red = 4; // red anode on pin 4
+int green = 5; // green anode on pin 5
+int blue = 6; // blue anode on pin 6
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
