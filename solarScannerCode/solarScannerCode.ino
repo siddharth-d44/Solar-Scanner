bd25eb8989f7ef63a7c981d7be4b82095af45880
Siddharth Deshpande <siddharth_d@icloud.com>
2025-07-07 18:30:59 -0500

    Adding the full code for my solar scanner project as well as a photo of the project

--- solarScannerCode/solarScannerCode.ino
+++ solarScannerCode/solarScannerCode.ino
@@ -3,9 +3,7 @@
 // creates a servo object
 Servo myServo;
 
-
 // declarations and initalizations for the ultrasonic sensor
-
 int trig = 10; // trig pin on pin 10
 int echo = 11; // trig pin on pin 11
 long dur; // variable for the time duration of the high signal
@@ -19,9 +17,10 @@ int green = 5; // green anode on pin 5
 int blue = 6; // blue anode on pin 6
 
 
-// this is my setup code
 void setup() {
   // put your setup code here, to run once:
+
+  // sets up inputs and outputs
   myServo.attach(serve);
   pinMode(trig, OUTPUT);
   pinMode(echo, INPUT);
@@ -32,25 +31,91 @@ void setup() {
 }
 
 
-// this code operates the scanner and loops over and over again
 void loop() {
   // put your main code here, to run repeatedly:
 
   // this for loop turns the servo motor half a counterclockwise rotation
   for (int deg = 0; deg <= 180; deg++) {
+
     myServo.write(deg); // turns the servo motor to the respective degree
-    
-    
-    delay(20);
+    delay(10); // delay of 10 ms between each turn in degree
+
+    // the following code emits waves and calculates distance
+    digitalWrite(trig, LOW); // eliminates sound
+    delay(10); // delays for 10 ms
+    digitalWrite(trig, HIGH); // emits sound waves 
+    delayMicroseconds(10); // delayes 10 us
+    digitalWrite(trig, LOW); // stops sound pulses
+    dur = pulseIn(echo, HIGH); // finds time duration
+    dist = dur * 0.017; // calculates distance from object
+
+    // executes if object is less than 10 cm from scanner
+    if (dist < 10)
+    {
+      digitalWrite(red, HIGH); // turns off red LED
+      digitalWrite(green, LOW); // turns the green light on when object is far away
+      digitalWrite(blue, LOW); // turns off blue LED
+      tone(buzz, 432); // plays high note
+    }
+
+    // executes if if object is more than 10 cm but less than 25 cm from scanner
+    else if (dist < 25)
+    {
+      digitalWrite(red, LOW); // turns off red LED
+      digitalWrite(green, HIGH); // turns the green light on when object is far away
+      digitalWrite(blue, LOW); // turns off blue LED
+      tone(buzz, 216); // plays lower note
+    }
+
+    // executes if distance of object is greater than 25 cm from scanner
+    else 
+    {
+      digitalWrite(red, LOW); // turns off red LED
+      digitalWrite(green, LOW); // turns the green light on when object is far away
+      digitalWrite(blue, HIGH); // turns off blue LED
+      noTone(buzz); // plays lower note
+    }
   }
 
   // this for loop turns the servo motor half  clockwise rotation
   for (int deg = 180; deg >= 0; deg--) {
     myServo.write(deg); // turns the servo motor to the respective degree
+    delay(10); // delay of 20 ms between each turn in degree
     
-    
-    
-    delay(20);
-  }
+    // the following code emits waves and calculates distance
+    digitalWrite(trig, LOW); // eliminates sound
+    delay(10); // delays for 10 ms
+    digitalWrite(trig, HIGH); // emits sound waves 
+    delayMicroseconds(10); // delayes 10 us
+    digitalWrite(trig, LOW); // stops sound pulses
+    dur = pulseIn(echo, HIGH); // finds time duration
+    dist = dur * 0.017; // calculates distance from object
 
+    // executes if object is less than 10 cm from scanner
+    if (dist < 10)
+    {
+      digitalWrite(red, HIGH); // turns off red LED
+      digitalWrite(green, LOW); // turns the green light on when object is far away
+      digitalWrite(blue, LOW); // turns off blue LED
+      tone(buzz, 432); // plays high note
+    }
+
+    // executes if if object is more than 10 cm but less than 25 cd from scanner
+    else if (dist < 25)
+    {
+      digitalWrite(red, LOW); // turns off red LED
+      digitalWrite(green, HIGH); // turns the green light on when object is far away
+      digitalWrite(blue, LOW); // turns off blue LED
+      tone(buzz, 216); // plays lower note
+    }
+
+    // executes if distance of object is greater than 25 cm from scanner
+    else 
+    {
+      digitalWrite(red, LOW); // turns off red LED
+      digitalWrite(green, LOW); // turns the green light on when object is far away
+      digitalWrite(blue, HIGH); // turns off blue LED
+      noTone(buzz); // plays lower note
+    }
+  }
 }
